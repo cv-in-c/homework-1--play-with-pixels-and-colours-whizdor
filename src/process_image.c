0ecc00794f9e5db0692d0dd22200c09780f28759
whizdor <118841840+whizdor@users.noreply.github.com>
2023-05-24 08:45:29 +0530

    Update process_image.c
    RGB to HCL Conversion

--- src/process_image.c
+++ src/process_image.c
@@ -81,17 +81,6 @@ void shift_image(image im, int c, float v)
     }
 }
 
-void scale_image(image im, int c, float v)
-{
-    for (int x = 0; x < im.w; x++)
-    {
-        for (int y = 0; y < im.h; y++)
-        {
-            set_pixel(im, x, y, c, (get_pixel(im, x, y, c)) * v);
-        }
-    }
-}
-
 void clamp_image(image im)
 {
     for (int x = 0; x < im.w; x++)
@@ -118,53 +107,36 @@ float three_way_min(float a, float b, float c)
 
 void rgb_to_hsv(image im)
 {
-    // image img = load_image("figs/dog.hsv.png");
-    // int p , q;
-    // p = 200;
-    // q = 200;
-    // printf("\n\n\n%f %f %f\n",get_pixel(im,p,q,0),get_pixel(im,p,q,1),get_pixel(im,p,q,2));
     for (int x = 0; x < im.w; x++)
         for (int y = 0; y < im.h; y++)
         {
-            float r, g, b;
-            r = get_pixel(im, x, y, 0);
-            g = get_pixel(im, x, y, 1);
-            b = get_pixel(im, x, y, 2);
+            float hue = 0.0, saturation, value;
 
-            float hue, saturation, value;
-            float c = three_way_max(r, g, b) - three_way_min(r, g, b);
+            value = three_way_max(get_pixel(im, x, y, 0), get_pixel(im, x, y, 1), get_pixel(im, x, y, 2));   // value
+            float m = three_way_min(get_pixel(im, x, y, 0), get_pixel(im, x, y, 1), get_pixel(im, x, y, 2)); // min
+            float c = value - m;
 
-            value = three_way_max(r, g, b); // value
-            if (value == 0)
-            {
-                saturation = 0;
-                hue = 0;
-            }
+            if (value <= 0)
+                saturation = 0.0;
             else
-            {
-                saturation = 1 - three_way_min(r, g, b) / three_way_max(r,g,b);
+                saturation = c / value;
 
-                if (value == r)
-                {
-                    hue = ((g - b) / c);
-                    if (b > g)
-                        hue += 6.0;
-                }
-                else if (value == g)
-                    hue = ((b - r) / c) + 2.0;
+            if (c > 0.0)
+            {
+                if (value == get_pixel(im, x, y, 0))
+                    hue = ((get_pixel(im, x, y, 1) - get_pixel(im, x, y, 2)) / c);
+                else if (value == get_pixel(im, x, y, 1))
+                    hue = ((get_pixel(im, x, y, 2) - get_pixel(im, x, y, 0)) / c) + 2.0;
                 else
-                    hue = ((r - g) / c) + 4.0;
+                    hue = ((get_pixel(im, x, y, 0) - get_pixel(im, x, y, 1)) / c) + 4.0;
             }
 
-            hue = hue / 6;
+            hue = (hue < 0.0) ? (hue / 6.0) + 1 : (hue / 6.0);
 
             set_pixel(im, x, y, 0, hue);
             set_pixel(im, x, y, 1, saturation);
             set_pixel(im, x, y, 2, value);
         }
-
-    // printf("%f %f %f\n",get_pixel(im,p,q,0),get_pixel(im,p,q,1),get_pixel(im,p,q,2));
-    // printf("%f %f %f\n",get_pixel(img,p,q,0),get_pixel(img,p,q,1),get_pixel(img,p,q,2));
 }
 
 void hsv_to_rgb(image im)
@@ -177,7 +149,6 @@ void hsv_to_rgb(image im)
             s = get_pixel(im, x, y, 1);
             v = get_pixel(im, x, y, 2);
 
-
             float r, g, b;
 
             float c = s * v;
@@ -186,37 +157,37 @@ void hsv_to_rgb(image im)
 
             h = h * 6.0;
 
-            if(v == 0)
+            if (v == 0)
             {
                 r = 0;
                 b = 0;
                 g = 0;
-            } 
-            else if(h < 1.0)
+            }
+            else if (h < 1.0)
             {
                 r = max;
                 b = min;
                 g = h * c + b;
             }
-            else if(h < 2.0)
+            else if (h < 2.0)
             {
                 g = max;
                 b = min;
                 r = b - (h - 2) * c;
             }
-            else if(h < 3.0)
+            else if (h < 3.0)
             {
                 g = max;
                 r = min;
                 b = (h - 2) * c + r;
             }
-            else if(h < 4.0)
+            else if (h < 4.0)
             {
                 b = max;
                 r = min;
                 g = r - (h - 4) * c;
             }
-            else if(h < 5.0)
+            else if (h < 5.0)
             {
                 b = max;
                 g = min;
@@ -224,18 +195,82 @@ void hsv_to_rgb(image im)
             }
             else
             {
-                 r = max;
-                 g = min;
-                 b = g - (h - 6) * c;
+                r = max;
+                g = min;
+                b = g - (h - 6) * c;
             }
 
             set_pixel(im, x, y, 0, r);
             set_pixel(im, x, y, 1, g);
             set_pixel(im, x, y, 2, b);
         }
-    
-    // int p , q;
-    // p = 200;
-    // q = 200;
-    // printf("%f %f %f\n",get_pixel(im,p,q,0),get_pixel(im,p,q,1),get_pixel(im,p,q,2));
+}
+
+void scale_image(image im, int c, float v)
+{
+    for (int x = 0; x < im.w; x++)
+    {
+        for (int y = 0; y < im.h; y++)
+        {
+            set_pixel(im, x, y, c, (get_pixel(im, x, y, c)) * v);
+        }
+    }
+}
+
+float f(float a)
+{
+    float del = 6.0/29.0;
+    if(a > del * del * del) return pow(a,1/3.0);
+    return (a/(3*del*del) + 2*del/3.0);
+}
+
+void rgb_to_hcl(image im)
+{
+    for (int x = 0; x < im.w; x++)
+        for (int y = 0; y < im.h; y++)
+        {
+            float R, G, B;
+            float r, g, b;
+            R = get_pixel(im, x, y, 0);
+            G = get_pixel(im, x, y, 1);
+            B = get_pixel(im, x, y, 2);
+
+            // Gamma Decompression
+            r = pow(R, 2.2);
+            g = pow(G, 2.2);
+            b = pow(B, 2.2);
+
+            // CIE-RGB Colour-Space to CIE-XYZ Conversion
+            // Matrix(M) sourced from http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+            //[X Y Z]' = M [R G B]'
+
+            float X = 0.4887180 * r + 0.3106803 * g + 0.2006017 * b;
+            float Y = 0.1762044 * r + 0.8129847 * g + 0.0108109 * b;
+            float Z = 0.0000000 * r + 0.0102048 * g + 0.9897952 * b;
+
+            // CIE-RGB Colour-Space to CIE-XYZ Conversion
+            // http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_Lab.html
+
+            float Xr,Yr,Zr;
+
+            // Assuming Reference White Point values ar Tc = 2000K,
+            Xr = 1.2743;
+            Yr = 1;
+            Zr = 0.1452;
+
+            float L,a,b;
+
+            L = 116 * f(Y/Yr) - 16;
+            a = 500 * (f(X/Xr) - f(Y/Yr));
+            b = 200 * (f(Y/Yr) - f(Z/Zr));
+
+            //Finally, Conversion to HCL Colour Space.
+
+            float H = (b/a > 0) ? tanh(b/a) : tanh(b/a) + 2*3.14;
+            float C = sqrt(a*a + b*b);
+
+            set_pixel(im, x, y, 0, H);
+            set_pixel(im, x, y, 1, C);
+            set_pixel(im, x, y, 2, L);
+        }
 }
